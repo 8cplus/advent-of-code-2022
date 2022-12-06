58458c728e7ee847f540648fda3369fe57cc19d7
Tiago Silva <Tiago.Silva@bjss.com>
2022-12-06 13:17:27 +0000

    day 6

--- day6/day6.go
+++ day6/day6.go
@@ -5,31 +5,21 @@ import (
 	"fmt"
 )
 
-func ex1AndEx2(packetsize int) int {
-	scanner := LoadFile("input/input6.txt")
-	scanner.Scan()
-	strIn := scanner.Text()
-
-	for i := packetsize; i < len(strIn); i++ {
-
-		found := true
-	out:
-		for j := i - packetsize; j < i; j++ {
-			for k := j + 1; k < i; k++ {
-				if strIn[j] == strIn[k] {
-					found = false
-					break out
-				}
+func findMarker(strIn string, packetSize, pos int) int {
+	for i := 0; i < packetSize; i++ {
+		for j := i + 1; j < packetSize; j++ {
+			if strIn[i] == strIn[j] {
+				return findMarker(strIn[1:], packetSize, pos+1)
 			}
 		}
-		if found {
-			return i
-		}
 	}
-	return -1
+	return pos
 }
 
 func Ex1andEx2() {
-	fmt.Println("Ex1: ", ex1AndEx2(4))
-	fmt.Println("Ex2: ", ex1AndEx2(14))
+	scanner := LoadFile("input/input6.txt")
+	scanner.Scan()
+
+	fmt.Println("Ex1: ", findMarker(scanner.Text(), 4, 4))
+	fmt.Println("Ex2: ", findMarker(scanner.Text(), 14, 14))
 }
