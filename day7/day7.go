5a9bffb08abf6b9d0a88bba45337f4dd7794afe2
Tiago Silva <Tiago.Silva@bjss.com>
2022-12-07 22:55:59 +0000

    day 7

--- day7/day7.go
+++ day7/day7.go
@@ -2,6 +2,7 @@ package day7
 
 import (
 	. "adventofcode2022/tools"
+	"bufio"
 	"fmt"
 	"strings"
 )
@@ -32,48 +33,34 @@ func (d *directory) spaceToFree(smallestDir, spaceNeeded int) int {
 	return smallestDir
 }
 
-func applyCommands(commands []string, currentDir *directory) (remainingCommands []string, size int) {
-
-	for len(commands) > 0 {
-		cmd := strings.Split(commands[0], " ")
+func applyCommands(scanner *bufio.Scanner, currentDir *directory) (size int) {
+	for scanner.Scan() {
+		cmd := strings.Split(scanner.Text(), " ")
 		if cmd[0][0:1] == "$" {
 			if cmd[1] == "cd" {
 				if cmd[2] == ".." { // cd ..
-					return commands[1:], currentDir.sizeOfFiles
+					return currentDir.sizeOfFiles
 				} else { // cd folder
-					subSize := 0
-					commands, subSize = applyCommands(commands[1:], currentDir.subDir[cmd[2]])
-					currentDir.sizeOfFiles += subSize
+					currentDir.sizeOfFiles += applyCommands(scanner, currentDir.subDir[cmd[2]])
 				}
-			} else { // ls
-				commands = commands[1:]
 			}
 		} else {
 			if cmd[0] == "dir" { // directory
 				currentDir.subDir[cmd[1]] = &directory{subDir: make(map[string]*directory)}
-				commands = commands[1:]
 			} else { // file
 				currentDir.sizeOfFiles += Atoi(cmd[0])
-				commands = commands[1:]
 			}
 		}
 	}
-	return nil, currentDir.sizeOfFiles
+	return currentDir.sizeOfFiles
 }
 
 func Ex1andEx2() {
 	scanner := LoadFile("input/input7.txt")
 	scanner.Scan() // first cd /
 	root := &directory{subDir: make(map[string]*directory)}
-	commands := []string{}
 
-	for scanner.Scan() {
-		strIn := scanner.Text()
-		commands = append(commands, strIn)
-	}
-
-	applyCommands(commands, root)
-
+	applyCommands(scanner, root)
 	fmt.Println("Ex1: ", root.sumSizesLessEqThan(100000))
 	fmt.Println("Ex2: ", root.spaceToFree(70000000, 30000000-(70000000-root.sizeOfFiles)))
 }
